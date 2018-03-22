8474d131383468eb5a0e9520350479b62e790c82
m305-11\w401 <moebb@ddasmail.com.tw>
2018-03-22 18:06:26 +0800

    no message

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (n < 1)
+            {
+
+                Random rand = new Random();
+
+                int r1 = rand.Next(1, 43);
+                int r2 = rand.Next(1, 43);
+                int r3 = rand.Next(1, 43);
+                int r4 = rand.Next(1, 43);
+                int r5 = rand.Next(1, 43);
+                int r6 = rand.Next(1, 43);
+
+                Console.WriteLine("本次開獎號碼：{0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r1, r2, r3, r4, r5, r6);
+                Console.ReadLine();
+            }
+        }
+    }
+}
